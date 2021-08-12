2117a997af7431c82366d05c84a06aca109de7fe
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-12 14:23:48 +0100

    save and delete all methods complete

--- start_code/repositories/biting_repository.py
+++ start_code/repositories/biting_repository.py
@@ -4,11 +4,12 @@ from models.biting import Biting
 def save(biting):
     zombie_id = biting.zombie.id
     human_id = biting.human.id
-    sql = "INSERT INTO bitings (zombie_id, human_id) VALUES (%s, %s)"
+    sql = "INSERT INTO bitings (zombie_id, human_id) VALUES (%s, %s) RETURNING *"
     values = [zombie_id, human_id]
-    return run_sql(sql, values)
-    # id = results[0]['id']
-    # biting.id = id
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    biting.id = id
+    return biting
 
 def delete_all():
     sql = "DELETE FROM bitings"
